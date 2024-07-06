b2517dc8207c49eb651e7e7fcad8fa1cf656723b
Kristjan Lääne <xonproject@gmail.com>
2024-07-06 14:54:57 +0300

    buttons

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -61,23 +61,35 @@ export default function Intro() {
         learner and up to anything development related.
       </motion.p>
 
-      <div className='flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium'>
+      <motion.div
+        className='flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium'
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.1,
+        }}
+      >
         <Link
           href='#contact'
-          className='bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full'
+          className='group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition'
         >
-          Contact me here <BsArrowRight />
+          Contact me here{' '}
+          <BsArrowRight className='opacity-70 group-hover:translate-x-1 transition' />
         </Link>
-        <a className='bg-white px-7 py-3 flex items-center gap-2 rounded-full'>
-          Download CV <HiDownload />
+
+        <a className='group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110  active:scale-105 transition cursor-pointer'>
+          Download CV{' '}
+          <HiDownload className='opacity-60 group-hover:translate-y-1 transition' />
         </a>
+
         <a className='bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full'>
           <BsLinkedin />
         </a>
+
         <a className='bg-white p-4 text-[1.35rem] text-gray-700 flex items-center gap-2 rounded-full'>
           <FaGithubSquare />
         </a>
-      </div>
+      </motion.div>
     </section>
   );
 }

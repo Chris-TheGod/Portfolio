e2b81c27e277376e168b92f50ae35d854d93f1df
Kristjan Lääne <xonproject@gmail.com>
2024-07-17 19:04:35 +0300

    email

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,32 +1,40 @@
 'use server';
 
-import { validateString } from '@/lib/utils';
-
+import React from 'react';
 import { Resend } from 'resend';
+import { validateString } from '@/lib/utils';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendEmail = async (formData: FormData) => {
-  const message = formData.get('message');
   const senderEmail = formData.get('senderEmail');
+  const message = formData.get('message');
 
-  if (!validateString(sendEmail, 500)) {
+  // simple server-side validation
+  if (!validateString(senderEmail, 500)) {
     return {
       error: 'Invalid sender email',
     };
   }
-
   if (!validateString(message, 5000)) {
     return {
       error: 'Invalid message',
     };
   }
 
-  resend.emails.send({
-    from: 'onboarding@resend.dev',
-    to: 'godgamerchris@gmail.com',
-    subject: 'Message from portfolio contact form',
-    reply_to: senderEmail,
-    text: message,
-  });
+  let data;
+  try {
+    data = await resend.emails.send({
+      from: 'Contact Form <onboarding@resend.dev>',
+      to: 'godgamerchris@gmail.com',
+      subject: 'Message from contact form',
+      reply_to: senderEmail as string,
+    });
+  } catch (error: unknown) {
+    console.log(error);
+  }
+
+  return {
+    data,
+  };
 };
